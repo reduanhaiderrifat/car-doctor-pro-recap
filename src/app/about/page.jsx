37416e094908ebb9ec25d2a7fe5a82e6537ff1dd
reduanhaiderrifat <reduanhaiderrifat@gmail.com>
2024-10-03 11:13:39 +0600

    add push notification

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,7 +1,41 @@
-import React from "react";
+"use client";
+import { useSession } from "next-auth/react";
+import React, { useCallback, useEffect } from "react";
 
-const page = () => {
-  return <div>this is about page</div>;
+const Page = () => {
+  const session = useSession();
+
+  const sendNotification = () => {
+    if ("Notification" in window && Notification.permission === "granted") {
+      new Notification("Hellow Developers", {
+        body: "This is a notification",
+        icon: `${session?.data?.user?.image}`,
+      });
+    }
+  };
+  const requestNotification = useCallback(() => {
+    if ("Notification" in window) {
+      Notification.requestPermission().then((permission) => {
+        if (permission === "granted") {
+          console.log("massege send");
+          sendNotification();
+        }
+      });
+    }
+  }, []);
+  useEffect(() => {
+    if ("Notification" in window) {
+      requestNotification();
+    }
+  }, [requestNotification]);
+  return (
+    <div>
+      this is about page{" "}
+      <button onClick={sendNotification} className="btn btn-primary">
+        Notification
+      </button>
+    </div>
+  );
 };
 
-export default page;
+export default Page;
