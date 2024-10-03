ebba67d4761d6433030a53707bcb2c20ec28f5b6
reduanhaiderrifat <reduanhaiderrifat@gmail.com>
2024-10-03 11:19:08 +0600

    add push notification

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -3,36 +3,43 @@ import { useSession } from "next-auth/react";
 import React, { useCallback, useEffect } from "react";
 
 const Page = () => {
-  const session = useSession();
+  const { data: session } = useSession();
 
   const sendNotification = () => {
     if ("Notification" in window && Notification.permission === "granted") {
-      new Notification("Hellow Developers", {
+      new Notification("Hello Developers", {
         body: "This is a notification",
-        icon: `${session?.data?.user?.image}`,
+        icon: session?.user?.image || "https://ibb.co.com/Tkq3PcP", // Fallback to a default icon
       });
     }
   };
+
   const requestNotification = useCallback(() => {
     if ("Notification" in window) {
-      Notification.requestPermission().then((permission) => {
-        if (permission === "granted") {
-          console.log("massege send");
-          sendNotification();
-        }
-      });
+      Notification.requestPermission()
+        .then((permission) => {
+          if (permission === "granted") {
+            console.log("Message sent");
+            sendNotification();
+          }
+        })
+        .catch((error) => {
+          console.error("Notification permission error:", error); // Handle errors
+        });
     }
   }, []);
+
   useEffect(() => {
     if ("Notification" in window) {
       requestNotification();
     }
   }, [requestNotification]);
+
   return (
     <div>
-      this is about page{" "}
+      <p>This is the about page</p>
       <button onClick={sendNotification} className="btn btn-primary">
-        Notification
+        Send Notification
       </button>
     </div>
   );
